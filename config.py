c8740713e5cc2a916b21099d71f945d9f1b7d692
Morgan <C5ipo7i@gmail.com>
2019-08-14 14:44:19 -0700

    feat: added config file for storing params

---
+++ config.py
@@ -0,0 +1,16 @@
+class Config(object):
+    def __init__(self): 
+        self.keys = {
+                    0:'style',
+                    1:'techniques',
+                    2:'height_friendly',
+                    3:'finish_location',
+                    4:'start_location',
+                    5:'locations',
+                    6:'risk',
+                    7:'intensity',
+                    8:'complexity',
+                    9:'intra_difficulty',
+                    10:'hold_sets',
+                    11:'grade'
+                }
