3a03ef651a27b45dd157e04a665b5a34f28334ec
Morgan Griffiths <C5ipo7i@gmail.com>
2019-08-26 14:23:47 -0700

    chore: now works with server_utils

--- server.py
+++ server.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import datetime
+import numpy as np
 from datetime import date
 from dateutil.relativedelta import relativedelta
 import pprint
 import pymongo
 import json
 from pymongo import MongoClient
+
+from server_utils import ServerUtilities
+from Utils.config import Config
+
 app = Flask(__name__)
 
 """
@@ -29,25 +34,38 @@ passes the fields,grades, routes (historical and active, minus the active locati
 @app.route('/plan')
 def data():
     N = 189
+    config = Config('math')
 
     plan = request.args.get('data')
+    # TODO
     # Get plan, get locations, get setters
+    # Pull the goal data
+    # Return routes according to loss and novelty.
     
     db = connect()
+    # Pull the route data
     routes,setters,fields,grades = get_collections(db)
     historical_routes,active_routes = separate_routes(routes)
+
+    # Instantiate the utils class
+    utils = ServerUtilities(routes,historical_routes,fields,config)
+
     # Restrict historical routes to the previous 6 months. 
     now = date.today()
     six_months = now - relativedelta(months=+6)
     six_months_routes = restrict_history_by_date(historical_routes,six_months)
     # Restrict historical routes to the previous N routes (sorted according to date)
     N_historical_routes = historical_routes[-N:]
-
+    
     locations = ['Titan North','Great Roof']
     location_routes = get_routes_by_location(routes,locations)
-    # json_obj = json.dumps(dictionary)
+    # Mask the fields in those locations
+    # create N routes given the novelty and goals
+    
     # return jsonify(json_obj)
-    return str(routes.find_one())
+    return 'Donezors!'
+
+
 
 def connect(): 
     client = MongoClient('localhost',27017)
@@ -74,12 +92,13 @@ def separate_routes(routes):
 def restrict_history_by_date(historical_routes,cutoff):
     date_routes = []
     for route in historical_routes:
-        print('route',route)
+        # print('route',route)
         try:
             if route['date'].date() > cutoff:
                 date_routes.append(route)
         except:
-            print('Route has no date')
+            # print('Route has no date')
+            pass
     return date_routes
 
 def get_routes_by_location(routes,locations):
@@ -89,6 +108,9 @@ def get_routes_by_location(routes,locations):
             pprint.pprint(route)
             masked_routes.append(route)
     return masked_routes
+
+def convert_routes_to_array(routes):
+    pass
 
 if __name__ == '__main__':
     app.run()
